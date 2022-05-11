f37ebe453d23a670162bd86f462cd78e876df5f0
Protoyt <p.viki02@gmail.com>
2022-05-11 18:20:20 +0200

    Save3

--- Projekt/src/main/java/hu/inf/unideb/Car.java
+++ Projekt/src/main/java/hu/inf/unideb/Car.java
@@ -1,16 +1,48 @@
 package hu.inf.unideb;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-public class Car {
+@Table(name="cars")
+public class Car implements Serializable{
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @Id
     private String license;
     private String type;
     private int money;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
