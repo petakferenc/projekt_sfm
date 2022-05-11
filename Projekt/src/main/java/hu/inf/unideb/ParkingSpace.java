82a61ced7c72cb237558603a586d1de3df409e4c
Protoyt <p.viki02@gmail.com>
2022-05-11 20:08:10 +0200

    save4

--- Projekt/src/main/java/hu/inf/unideb/ParkingSpace.java
+++ Projekt/src/main/java/hu/inf/unideb/ParkingSpace.java
@@ -1,4 +1,29 @@
 package hu.inf.unideb;
 
-public class ParkingSpace {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="ParkingSpace")
+public class ParkingSpace implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @OneToOne
+    @JoinColumn(name = "Car_License", referencedColumnName = "license")
+    private Car car;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @Enumerated(EnumType.STRING)
+    private Type type;
+    private java.time.LocalDate date;
+
+    public enum Status{
+        FREE,USE,RENT
+    }
+    public enum Type{
+        CAR,BUS,DISABLED
+    }
+
+
 }
