fcbae3448de5a6cddf9f28d84118a0a2c9e8a285
ariotkiv <czireviktoria@gmail.com>
2022-05-16 13:28:41 +0200

    Add files via upload

---
+++ Projekt_UI_ver2/src/main/java/hu/inf/unideb/FXMLController.java
@@ -0,0 +1,122 @@
+package hu.inf.unideb;
+
+import java.net.URL;
+import java.text.BreakIterator;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+
+public class FXMLController implements Initializable {
+
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private ProgressBar progressBar2;
+    @FXML
+    private Label dateIn;
+    @FXML
+    private Label freeSpaces;
+    @FXML
+    private Label fee;
+    @FXML
+    private TextField addPass;
+    @FXML
+    private TextField licensePlate;
+    @FXML
+    private TextField getLicensePlate2;
+    @FXML
+    private Circle redCircle;
+    @FXML
+    private Circle greenCircle;
+    @FXML
+    private Circle redCircle2;
+    @FXML
+    private Circle greenCircle2;
+
+
+    private void Timenow(){
+        Thread thread = new Thread(() -> {
+            DateFormat df = new SimpleDateFormat("yyyy/MM/dd \n HH:mm:ss");
+            while(true){
+                try{
+                    Thread.sleep(1000);
+                }catch(Exception e){
+                    System.out.println(e);
+                }
+                final String timenow = df.format(new Date());
+                Platform.runLater(() -> {
+                    dateIn.setText(timenow); // This is the label
+                });
+            }
+        });
+        thread.start();
+    }
+    private void Loading(){
+        Thread thread = new Thread(() -> {
+
+            while (progressBar.getProgress() < 1) {
+                try {
+                    Thread.sleep(1);
+                    progressBar.setProgress(progressBar.getProgress() + 0.001);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+            if (((int)progressBar.getProgress()) == 1) {
+                trafficLight.setGreen(redCircle, greenCircle);
+            }
+            try {
+                Thread.sleep(5000);
+                trafficLight.setRed(redCircle, greenCircle);
+                progressBar.setProgress(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        redCircle.setFill(Color.valueOf("#ff1c02"));  //piros: #ff1c02 szurke: #948a89 zold: #03ff0a
+        greenCircle.setFill(Color.valueOf("#948a89"));
+        Timenow();
+    }
+
+
+
+    @FXML
+    private void handleTicketButtonPushed(ActionEvent actionEvent){
+        progressBar.setProgress(0);
+        Loading();
+    }
+    public void handlePassButtonPushed(ActionEvent actionEvent) {
+        progressBar.setProgress(0);
+        Loading();
+    }
+
+    public void handleOkButtonPushed(ActionEvent actionEvent) {
+    }
+
+    public void handleLogButtonPushed(ActionEvent actionEvent) {
+    }
+
+    public void addPassButton(ActionEvent actionEvent) {
+    }
+
+    public void blackListButton(ActionEvent actionEvent) {
+    }
+}
+
---
+++ Projekt_UI_ver2/src/main/java/hu/inf/unideb/indicator.java
@@ -0,0 +1,18 @@
+package hu.inf.unideb;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+public class indicator {
+
+    public static void setGreen(Circle red, Circle green){
+        red.setFill(Color.valueOf("#948a89"));  //piros: #ff1c02 szurke: #948a89 zold: #03ff0a
+        green.setFill(Color.valueOf("#03ff0a"));
+    }
+    public static void setRed(Circle red, Circle green){
+        red.setFill(Color.valueOf("#ff1c02"));  //piros: #ff1c02 szurke: #948a89 zold: #03ff0a
+        green.setFill(Color.valueOf("#948a89"));
+    }
+}
+
