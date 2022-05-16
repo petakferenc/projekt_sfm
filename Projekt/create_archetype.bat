1558a70dd88897e8023d9ff4ad0f6afbe95af7ae
balazssajo <83130069+balazssajo@users.noreply.github.com>
2022-05-16 14:18:50 +0300

    Delete Projekt directory

--- Projekt/create_archetype.bat
+++
@@ -1,1 +0,0 @@
-mvn -U clean archetype:create-from-project -Dinteractive=false  -DkeepParent=true  -DpropertyFile=archetype.properties  -DpackageName=hu.unideb.inf -Darchetype.filteredExtensions=java,xml,md
