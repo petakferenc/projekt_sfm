183b01607717bc4602aa57c2386dc2e18152a1d2
balazssajo <83130069+balazssajo@users.noreply.github.com>
2022-05-17 21:18:58 +0200

    Add files via upload

--- create_archetype.bat
+++ create_archetype.bat
@@ -1,1 +1,1 @@
-mvn -U clean archetype:create-from-project -Dinteractive=false  -DkeepParent=true  -DpropertyFile=archetype.properties  -DpackageName=hu.unideb.inf -Darchetype.filteredExtensions=java,xml,md
+mvn -U clean archetype:create-from-project -Dinteractive=false  -DkeepParent=true  -DpropertyFile=archetype.properties   -DpackageName=hu.unideb.inf -Darchetype.filteredExtensions=java,xml,md
